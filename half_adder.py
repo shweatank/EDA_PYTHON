3f23ed5b3b12c0de132396eaf0b11d74de32f306
Vismithagowda123 <vismitha@mirafra.com>
2025-03-27 10:03:35 +0530

    day3_assign

---
+++ half_adder.py
@@ -0,0 +1,10 @@
+def half_adder_sum(a,b):
+    s= a ^ b
+    return s
+def half_adder_carry(a,b):
+    c= a and b
+    return c
+print(f'A B Sum Carry')
+for i in range(0,2):
+    for j in range(0,2):
+        print(f'{i} {j}  {half_adder_sum(i,j)}   {half_adder_carry(i,j)}')
