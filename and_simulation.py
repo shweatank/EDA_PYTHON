68ea3b45a5b9beb7ead34609599f758b3a4e5562
Deepika5940 <deepikadoddapaneni@mirafra.com>
2025-03-28 09:19:08 +0530

    gates_simulation

---
+++ and_simulation.py
@@ -0,0 +1,41 @@
+from myhdl import *
+def and_gate(a,b,c):
+    """
+    Basic and gate:Implements a simple two-input and gate.
+    a,b->Inputs
+    c->Output
+    """
+    @always_comb
+    def logic():
+        c.next = a & b
+    return logic
+
+#simulation
+def simulate_and_gate():
+    a,b,c = [Signal(bool(0)) for _ in range(3)]
+
+    def bench():
+        and_inst = and_gate(a,b,c)
+
+        @instance
+        def stimulus():
+            print("a b | c")
+            print("---------")
+            for i in range(2):
+                for j in range(2):
+                    a.next,b.next = i, j
+                    yield delay(10)
+                    print(f"{int(a)} {int(b)} | {int(c)}")
+
+        return and_inst, stimulus
+
+    #Generate VCD File for waveform analysis
+    tb = traceSignals(bench)
+    sim = Simulation(tb)
+    sim.run()
+    print("VCD file generated as 'and_gate.vcd'.")
+
+#Run simulation
+def test():
+    simulate_and_gate()
+test()
