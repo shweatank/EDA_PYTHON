3fb593b67219dd0d4dd7a3041bea124d555fc035
Deepika5940 <deepikadoddapaneni@mirafra.com>
2025-04-01 10:58:13 +0530

    Delete Half_adder.py

--- Half_adder.py
+++
@@ -1,8 +0,0 @@
-def half_adder(a,b):
-    return a^b
-def half_adder_carry(a,b):
-    c= a and b
-    return c
-for a in range(0,2):
-    for b in range(0,2):
-        print(f"{a}  {b}    {half_adder(a,b)}        {half_adder_carry(a,b)}")
